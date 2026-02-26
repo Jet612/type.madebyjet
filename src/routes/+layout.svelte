ecd5ef1c84f8da1b473012904df5c232d58c6284
Jet612 <jetflyer1130@gmail.com>
2026-02-26 14:59:00 -0500

    New Favicon
    Old one was corrupt I guess

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,7 @@
 <script lang="ts">
 	import './layout.css';
-	import favicon from '$lib/assets/favicon.png';
 
 	let { children } = $props();
 </script>
 
-<svelte:head><link rel="icon" type="image/png" href={`${favicon}?v=2`} /></svelte:head>
 {@render children()}
